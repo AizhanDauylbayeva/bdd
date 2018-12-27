646c69ff8c293e68c4eb5285a99e47f33b1a3ab8
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2018-12-27 10:53:37 +0600

    changed step definition

---
+++ src/test/java/com/cucumber/steps/stepDefinition.java
@@ -0,0 +1,60 @@
+package com.cucumber.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import framework.common.bo.User;
+import framework.common.pages.CloudPage;
+import framework.common.pages.HomePage;
+import framework.common.pages.InboxPage;
+import framework.core.driver.Driver;
+import framework.core.util.Waiting;
+import org.junit.AfterClass;
+import org.testng.Assert;
+
+public class stepDefinition {
+    private InboxPage inbox;
+    private HomePage homePage;
+    private CloudPage cloudPage;
+    private User user = new User();
+
+    @Given("^an open browser with mail.ru$")
+    public void open(){
+        homePage = new HomePage();
+        homePage.open();
+    }
+
+    @When("^fill username \"(.*)\" and password \"(.*)\"$")
+    public void fillInput(String username, String pass){
+        user.setUsername(username);
+        user.setPassword(pass);
+        homePage.fillUsername(username).fillPassword(pass);
+    }
+
+    @Then("^click and reach to the inbox page$")
+    public void signIn() {
+        inbox = homePage.signIn();
+        Assert.assertTrue(inbox.getUserName().equals(user.getUsername()));
+    }
+
+    @Given("^a button 'Облако'$")
+    public void waitForButtonIsEnabled(){
+        Waiting.waitForElementVisibleEnabled(inbox.getCloudButton());
+    }
+
+    @When("^click the button$")
+    public void openCloudPage() {
+        cloudPage = inbox.openCloudPage();
+    }
+
+    @Then("^reach to cloud page with a title \"(.*)\"$")
+    public void assertTitle(String title){
+        Assert.assertEquals(cloudPage.getTitle(), title);
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        Driver.getWebDriverInstance().quit();
+    }
+
+}
